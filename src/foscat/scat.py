a8c5dddcf3fc3cafa7c1ec4d3c088aeb515c5b47
Delouis Jean-Marc <jdelouis@mesopsl.obspm.fr>
2023-03-17 11:39:36 +0100

    Add S0 to scat

--- src/foscat/scat.py
+++ src/foscat/scat.py
@@ -3,16 +3,20 @@ import numpy as np
 
 
 def read(filename):
-    thescat=scat(1,1,1)
+    thescat=scat(1,1,1,1)
     return thescat.read(filename)
     
 class scat:
-    def __init__(self,p00,s1,s2,cross=False):
+    def __init__(self,p00,s0,s1,s2,cross=False):
         self.P00=p00
+        self.S0=s0
         self.S1=s1
         self.S2=s2
         self.cross=cross
         
+    def get_S0(self):
+        return(self.S0)
+
     def get_S1(self):
         return(self.S1)
     
@@ -31,10 +35,12 @@ class scat:
         
         if isinstance(other, scat):
             return scat((self.P00+ other.P00), \
+                        (self.S0+ other.S0), \
                         (self.S1+ other.S1), \
                         (self.S2+ other.S2))
         else:
             return scat((self.P00+ other), \
+                        (self.S0+ other), \
                         (self.S1+ other), \
                         (self.S2+ other))
 
@@ -44,10 +50,12 @@ class scat:
         
         if isinstance(other, scat):
             return scat((self.P00/ other.P00), \
+                        (self.S0/ other.S0), \
                         (self.S1/ other.S1), \
                         (self.S2/ other.S2))
         else:
             return scat((self.P00/ other), \
+                        (self.S0/ other), \
                         (self.S1/ other), \
                         (self.S2/ other))
         
@@ -58,10 +66,12 @@ class scat:
         
         if isinstance(other, scat):
             return scat((other.P00/ self.P00), \
+                        (other.S0 / self.S0), \
                         (other.S1 / self.S1), \
                         (other.S2 / self.S2))
         else:
             return scat((other/ self.P00), \
+                        (other / self.S0), \
                         (other / self.S1), \
                         (other / self.S2))
         
@@ -71,10 +81,12 @@ class scat:
         
         if isinstance(other, scat):
             return scat((self.P00- other.P00), \
+                        (self.S0- other.S0), \
                         (self.S1- other.S1), \
                         (self.S2- other.S2))
         else:
             return scat((self.P00- other), \
+                        (self.S0- other), \
                         (self.S1- other), \
                         (self.S2- other))
         
@@ -84,10 +96,12 @@ class scat:
         
         if isinstance(other, scat):
             return scat((self.P00* other.P00), \
+                        (self.S0* other.S0), \
                         (self.S1* other.S1), \
                         (self.S2* other.S2))
         else:
             return scat((self.P00* other), \
+                        (self.S0* other), \
                         (self.S1* other), \
                         (self.S2* other))
 
@@ -101,21 +115,28 @@ class scat:
         if hold:
             plt.figure(figsize=(8,8))
         
-        plt.subplot(3,1,1)
+        plt.subplot(2,2,1)
+        if legend:
+            plt.plot(self.get_np(abs(self.S0)).flatten(),color=color,label=r'%s $S_0$'%(name),lw=lw)
+        else:
+            plt.plot(self.get_np(abs(self.S0)).flatten(),color=color,lw=lw)
+        plt.yscale('log')
+        plt.legend()
+        plt.subplot(2,2,2)
         if legend:
             plt.plot(self.get_np(abs(self.S1)).flatten(),color=color,label=r'%s $S_1$'%(name),lw=lw)
         else:
             plt.plot(self.get_np(abs(self.S1)).flatten(),color=color,lw=lw)
         plt.yscale('log')
         plt.legend()
-        plt.subplot(3,1,2)
+        plt.subplot(2,2,3)
         if legend:
             plt.plot(self.get_np(abs(self.P00)).flatten(),color=color,label=r'%s $P_{00}$'%(name),lw=lw)
         else:
             plt.plot(self.get_np(abs(self.P00)).flatten(),color=color,lw=lw)
         plt.yscale('log')
         plt.legend()
-        plt.subplot(3,1,3)
+        plt.subplot(2,2,4)
         if legend:
             plt.plot(self.get_np(abs(self.S2)).flatten(),color=color,label=r'%s $S_2$'%(name),lw=lw)
         else:
@@ -124,15 +145,17 @@ class scat:
         plt.legend()
         
     def save(self,filename):
+        np.save('%s_s0.npy'%(filename), self.get_S0().numpy())
         np.save('%s_s1.npy'%(filename), self.get_S1().numpy())
         np.save('%s_s2.npy'%(filename), self.get_S2().numpy())
         np.save('%s_p0.npy'%(filename), self.get_P00().numpy())
         
     def read(self,filename):
+        s0=np.load('%s_s0.npy'%(filename))
         s1=np.load('%s_s1.npy'%(filename))
         s2=np.load('%s_s2.npy'%(filename))
         p0= np.load('%s_p0.npy'%(filename))
-        return scat(p0,s1,s2)
+        return scat(p0,s0,s1,s2)
     
     def get_np(self,x):
         if isinstance(x, np.ndarray):
@@ -141,10 +164,10 @@ class scat:
             return x.numpy()
 
     def std(self):
-        return np.sqrt(((abs(self.get_np(self.S1)).std())**2+(abs(self.get_np(self.S2)).std())**2+(abs(self.get_np(self.P00)).std())**2)/3)
+        return np.sqrt(((abs(self.get_np(self.S0)).std())**2+(abs(self.get_np(self.S1)).std())**2+(abs(self.get_np(self.S2)).std())**2+(abs(self.get_np(self.P00)).std())**2)/4)
 
     def mean(self):
-        return abs(self.get_np(self.S1).mean()+self.get_np(self.S2).mean()+self.get_np(self.P00).mean())/3
+        return abs(self.get_np(self.S0).mean()+self.get_np(self.S1).mean()+self.get_np(self.S2).mean()+self.get_np(self.P00).mean())/3
         
         
     
@@ -227,6 +250,14 @@ class funct(FOC.FoCUS):
             l_image1=I1
             if cross:
                 l_image2=I2
+
+        s0=None
+        s0 = self.bk_masked_mean(l_image1,vmask,axis=axis)
+        if cross:
+            if len(image1.shape)==1 or (len(image1.shape)==3 and isinstance(image1,FOC.Rformat)):
+                s0 = self.bk_concat([s0,self.bk_masked_mean(l_image2,vmask,axis=axis)],1)
+            else:
+                s0 = self.bk_concat([s0,self.bk_masked_mean(l_image2,vmask,axis=axis)],0)
 
         s1=None
         s2=None
@@ -352,44 +383,50 @@ class funct(FOC.FoCUS):
                         l2_image_imag = self.ud_grade_2(l2_image_imag,axis=axis)
 
         if len(image1.shape)==1 or (len(image1.shape)==3 and isinstance(image1,FOC.Rformat)):
-            return(scat(p00[0],s1[0],s2[0],cross))
+            return(scat(p00[0],s0[0],s1[0],s2[0],cross))
 
-        return(scat(p00,s1,s2,cross))
+        return(scat(p00,s0,s1,s2,cross))
 
     def square(self,x):
         # the abs make the complex value usable for reduce_sum or mean
         return scat(self.bk_abs(self.bk_square(x.P00)),
+                    self.bk_abs(self.bk_square(x.S0)),
                     self.bk_abs(self.bk_square(x.S1)),
                     self.bk_abs(self.bk_square(x.S2)))
 
     def reduce_mean(self,x,axis=None):
         if axis is None:
             return  scat(self.bk_reduce_mean(x.P00),
+                         self.bk_reduce_mean(x.S0),
                          self.bk_reduce_mean(x.S1),
                          self.bk_reduce_mean(x.S2))
         else:
             return scat(self.bk_reduce_mean(x.P00,axis=axis),
+                        self.bk_reduce_mean(x.S0,axis=axis),
                         self.bk_reduce_mean(x.S1,axis=axis),
                         self.bk_reduce_mean(x.S2,axis=axis))
 
     def reduce_sum(self,x,axis=None):
         if axis is None:
             return  self.bk_reduce_sum(x.P00)+ \
+                self.bk_reduce_sum(x.S0)+ \
                 self.bk_reduce_sum(x.S1)+ \
                 self.bk_reduce_sum(x.S2)
         else:
             return scat(self.bk_reduce_sum(x.P00,axis=axis),
+                        self.bk_reduce_sum(x.S0,axis=axis),
                         self.bk_reduce_sum(x.S1,axis=axis),
                         self.bk_reduce_sum(x.S2,axis=axis))
             
 
     def log(self,x):
         return scat(self.bk_log(x.P00),
+                    self.bk_log(x.S0),
                     self.bk_log(x.S1),
                     self.bk_log(x.S2))
     def inv(self,x):
-        return scat(1/(x.P00),1/(x.S1),1/(x.S2))
+        return scat(1/(x.P00),1/(x.S0),1/(x.S1),1/(x.S2))
 
     def one(self):
-        return scat(1.0,1.0,1.0)
+        return scat(1.0,1.0,1.0,1.0)
         
