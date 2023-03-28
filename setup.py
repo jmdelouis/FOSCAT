a7da471dd41426fc0dacc4af56490ede6202eb06
jd22d11 <jmdeloui@smtp.ifremer.fr>
2023-03-28 14:50:32 +0200

    debug doc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='foscat',
     version='1.9.2',
     description='Synthesise 2D or Healpix data using Cross Scattering Transform' ,
-    long_description='Synthesise data (2D or Healpix) using Cross Scattering Transform (https://arxiv.org/abs/2207.12527) usable for component separation (e.g. denoising). \n Demo package here : git@github.com:jmdelouis/FOSCAT_DEMO.git' ,
+    long_description='Synthesise data (2D or Healpix) using Cross Scattering Transform (https://arxiv.org/abs/2207.12527) usable for component separation (e.g. denoising). \n Demo package here : https://github.com/jmdelouis/FOSCAT_DEMO ' ,
     license='MIT',
     author='Jean-Marc DELOUIS',
     author_email='jean.marc.delouis@ifremer.fr',
