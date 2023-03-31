e1d00eabb348b0cf313bb0aceca0a9f097607677
jd22d11 <jmdeloui@smtp.ifremer.fr>
2023-03-31 15:12:02 +0200

    release candidate

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     name='foscat',
     version='1.9.6',
     description='Generate synthetic Healpix or 2D data using Cross Scattering Transform' ,
-    long_description='Utilize the Cross Scattering Transform (described in https://arxiv.org/abs/2207.12527) to synthesize Healpix or 2D data that is suitable for component separation purposes, such as denoising. \n A demo package for this process can be found at https://github.com/jmdelouis/FOSCAT_DEMO. ' ,
+    long_description='Utilize the Cross Scattering Transform (described in https://arxiv.org/abs/2207.12527) to synthesize Healpix or 2D data that is suitable for component separation purposes, such as denoising. \n A demo package for this process can be found at https://github.com/jmdelouis/FOSCAT_DEMO. \n Developed by : J.-M. Delouis, T. Foulqieur, L. Mousset, E. Allys ' ,
     license='MIT',
     author='Jean-Marc DELOUIS',
     author_email='jean.marc.delouis@ifremer.fr',
-    maintainer=['Theo Foulquier','Louise Mousset']
-    maintainer_email=['theo.foulquier@ifremer.fr','louise.mousset@irap.omp.eu']
+    maintainer='Theo Foulquier',
+    maintainer_email='theo.foulquier@ifremer.fr',
     packages=['foscat'],
     package_dir={'': 'src'},
     url='https://github.com/jmdelouis/FOSCAT',
--- sh.sh
+++ sh.sh
@@ -1,4 +1,3 @@
 rm dist/*
-python convdemo.py
 python3 -m build
 python3 -m twine upload --repository foscat dist/*
