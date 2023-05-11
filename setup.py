7e4d1ea39b3c188f8b0d832f9b35f20a1cd533b6
jd22d11 <jmdeloui@smtp.ifremer.fr>
2023-05-11 16:31:53 +0200

    correc the developer name

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='foscat',
-    version='2.0.2',
+    version='2.0.3',
     description='Generate synthetic Healpix or 2D data using Cross Scattering Transform' ,
-    long_description='Utilize the Cross Scattering Transform (described in https://arxiv.org/abs/2207.12527) to synthesize Healpix or 2D data that is suitable for component separation purposes, such as denoising. \n A demo package for this process can be found at https://github.com/jmdelouis/FOSCAT_DEMO. \n\n List of developers : J.-M. Delouis, T. Foulqieur, L. Mousset, E. Allys ' ,
+    long_description='Utilize the Cross Scattering Transform (described in https://arxiv.org/abs/2207.12527) to synthesize Healpix or 2D data that is suitable for component separation purposes, such as denoising. \n A demo package for this process can be found at https://github.com/jmdelouis/FOSCAT_DEMO. \n\n List of developers : J.-M. Delouis, T. Foulquier, L. Mousset, E. Allys ' ,
     license='MIT',
     author='Jean-Marc DELOUIS',
     author_email='jean.marc.delouis@ifremer.fr',
