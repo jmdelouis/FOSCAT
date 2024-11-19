8db50bffc7c1072e9fb438cb04c8db7e1c20388c
Jean-Marc Delouis <119950646+jmdelouis@users.noreply.github.com>
2024-11-19 16:10:42 +0100

    Update src/foscat/xarray/parameters.py
    Co-authored-by: Justus Magin <keewis@users.noreply.github.com>

--- src/foscat/xarray/parameters.py
+++ src/foscat/xarray/parameters.py
@@ -17,10 +17,15 @@ class Parameters:
     kernel_size : int
         The size of the kernel in cells / pixels
     jmax_delta : float
-        Typically, the scattering covariance coefficients are calculated over N scales,
-        starting from the smallest scale (j=0) up to the largest scale (j_{\text{max}}=\log_2(\text{nside})).
-        Adjusting this parameter eliminates the coefficients at the largest scale
-        where j_{\text{max}}=\log_2(\text{nside})-j_{\text{max\_delta}}.
+        Compute the scattering covariance coefficients for this many refinement levels,
+        starting with the data level (``level``, which is defined as ``log2(nside)``) in
+        decreasing order. This means that the levels for which coefficients are computed
+        are in the range
+        
+        .. math::
+            [level - relative_level, level]
+        
+        If ``relative_level`` is None, compute all levels until ``level=0``.
     dtype : str or numpy.dtype, default: "float64"
         The dtype to use for the transform.
     backend : {"numpy", "torch", "tensorflow"}, default: "tensorflow"
