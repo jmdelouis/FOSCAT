ae04163f48a97cee408d8e081efdbc3a8b09c6a0
jmdelouis <jmdeloui@W-IFR-23-P014.ifremer.fr>
2025-03-19 11:37:44 +0100

    initilise the branch

--- src/foscat/FoCUS.py
+++ src/foscat/FoCUS.py
@@ -35,7 +35,7 @@ class FoCUS:
         mpi_rank=0,
     ):
 
-        self.__version__ = "3.8.2"
+        self.__version__ = "3.9.0"
         # P00 coeff for normalization for scat_cov
         self.TMPFILE_VERSION = TMPFILE_VERSION
         self.P1_dic = None
