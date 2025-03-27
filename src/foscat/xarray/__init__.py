216acec7d1597918852a1e800e9525bbd0f3265d
Justus Magin <keewis@users.noreply.github.com>
2025-03-27 19:17:30 +0100

    maintenance of the `xarray` interface (#27)
    * expect `obj.backend.BACKEND` to be a string
    
    * don't plot variances
    
    * expose the cross statistics
    
    * make `parameters` keyword-only
    
    * demo the use of the xarray API
    
    * remove the obsolete `backends` lookup table
    
    * put the other dims into the metadata
    
    * fix the plotting for spatial images
    
    * remove the demo notebook

--- src/foscat/xarray/__init__.py
+++ src/foscat/xarray/__init__.py
@@ -1,8 +1,9 @@
 from foscat.xarray.accessor import FoscatAccessor  # noqa: F401
 from foscat.xarray.parameters import Parameters
-from foscat.xarray.statistics import reference_statistics
+from foscat.xarray.statistics import cross_statistics, reference_statistics
 
 __all__ = [
     "Parameters",
     "reference_statistics",
+    "cross_statistics",
 ]
