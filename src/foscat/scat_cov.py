69574860b541ec90917bf14df7218f098b2e5cbe
jmdelouis <jmdeloui@W-IFR-23-P014.ifremer.fr>
2025-05-19 14:29:28 +0200

    Debug cmat2

--- src/foscat/scat_cov.py
+++ src/foscat/scat_cov.py
@@ -2441,7 +2441,7 @@ class funct(FOC.FoCUS):
             )
 
             for k2 in range(k + 1):
-                tmp2 = self.backend.bk_repeat(sim, self.NORIENT, axis=-2)
+                tmp2 = self.backend.bk_tile(sim, self.NORIENT, axis=-2)
                 sim2 = self.backend.bk_reduce_sum(
                     self.backend.bk_reshape(
                         self.backend.bk_cast(
