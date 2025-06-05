b0a679de8dca8ddd30250b93708c42444f542903
jmdelouis <jmdeloui@W-IFR-23-P014.ifremer.fr>
2025-06-05 17:34:33 +0200

    Change version

--- src/foscat/FoCUS.py
+++ src/foscat/FoCUS.py
@@ -35,7 +35,7 @@ class FoCUS:
         mpi_rank=0,
     ):
 
-        self.__version__ = "2025.05.2"
+        self.__version__ = "2025.06.1"
         # P00 coeff for normalization for scat_cov
         self.TMPFILE_VERSION = TMPFILE_VERSION
         self.P1_dic = None
