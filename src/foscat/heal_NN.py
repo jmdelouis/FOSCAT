e3ed50ff08ab78dd06d35abbb28c12fc351c750d
jmdelouis <jmdeloui@W-IFR-23-P014.ifremer.fr>
2025-06-16 15:56:13 +0200

    add cnn layer degrade

--- src/foscat/heal_NN.py
+++ src/foscat/heal_NN.py
@@ -223,7 +223,7 @@ class CNN:
         im = self.backend.bk_reduce_sum(self.backend.bk_reshape(im,[im.shape[0],im.shape[1],self.NORIENT,im.shape[3]//4,4]),4)
 
         if self.add_undersample_data:
-            l_im=self.backend.bk_repeat(self.backend.bk_reshape(in_im,[in_im.shape[0],in_im.shape[1],1,l_im.shape[2]]),2)
+            l_im=self.backend.bk_repeat(self.backend.bk_reshape(in_im,[in_im.shape[0],in_im.shape[1],1,in_im.shape[2]]),2)
             l_im=self.backend.bk_reduce_sum(
                                            self.backend.bk_reshape(in_im,[in_im.shape[0],in_im.shape[1]+1,self.NORIENT,l_im.shape[2]//4,4]), 4)
             im=self.backend.bk_concat([im,l_im],1)
